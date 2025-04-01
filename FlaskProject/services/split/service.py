b28dcd82f50eee902016d7dcd9186dcb539f31ef
Sibo Guo <sguo0800@uni.sydney.edu.au>
2025-04-02 10:54:07 +1100

    Create service.py
    #74

---
+++ FlaskProject/services/split/service.py
@@ -0,0 +1,9 @@
+from .webpage_split import extract_section
+
+class ExtractSection:
+    def __init__(self):
+        self.sections = {'Collect': None, 'Use': None, 'Share': None}
+
+    def split(self, page):
+        self.sections = extract_section(page)
+        return self.sections
