482303d1903d62339408b6fbb6cadfb6d49686fb
Shijiao Han <shan0974@uni.sydney.edu.au>
2025-04-02 16:17:32 +1100

    integrate of llm classification
    #72

--- FlaskProject/utils/text_processor.py
+++ FlaskProject/utils/text_processor.py
@@ -1,3 +1,7 @@
+import os
+import re
+import json
+
 class TextProcessor:
     def preprocess(self, text):
         
@@ -15,3 +19,21 @@ class TextProcessor:
         if not text.endswith(('.', '!', '?')):
             text += '.'
         return text 
+    
+    # Used in llm_privacy_classification.classification_service.py
+    def load_text_file(self, folder_path, filename):
+        file_dict = {}
+        if filename.endswith(".json"):
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, "r", encoding="utf-8") as file:
+                try:
+                    file_dict[filename] = json.load(file)
+                except json.JSONDecodeError:
+                    print(f"Error decoding JSON in {filename}")
+        return file_dict
+
+    # Used in llm_privacy_classification.llm_classification_service.py
+    def clean_json_string(self, json_str):
+        json_str = re.sub(r"```json\s*", "", json_str)
+        json_str = re.sub(r"```", "", json_str)
+        return json_str.strip()
